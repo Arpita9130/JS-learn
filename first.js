a8ef9b6d0296651b38209b3cb60aee825b8ba424
arpita <arpitawaghode2002@gmail.com>
2024-06-22 13:23:06 +0530

    rename folder

--- first.js
+++ first.js
@@ -1,1 +1,82 @@
-console.log("hello");
+/*const score=400
+console.log(score);
+const balance=new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+const other=123.986
+console.log(other.toPrecision(4));
+const hr=900
+console.log(hr.toLocaleString('en-IN'));*/
+//
+ //let mydate=new Date(2023,1,0);
+ //console.log(mydate.toString());
+//  console.log(mydate.toISOString());
+//  console.log(mydate.toLocaleString());
+//  console.log(typeof mydate);
+
+// const regularuser={
+//   fullname:{
+//   user:{
+//     firstname:"Arpita",
+//     lastname:"Waghode"
+//   }
+// }}
+
+// console.log(regularuser.fullname.user.firstname);
+
+// const myArray=[10,20,40]
+// const myArray2=[20,30,50,70]
+// const newArray=myArray.concat(myArray2)
+// console.log(newArray);
+
+// const obj1={1:"a",2:"b"}
+// const obj2={3:"a",4:"c"}
+// const obj3={...obj1,...obj2}
+// //Object.assign({},obj1,obj2)
+// console.log(obj3);
+
+// const course={
+//   coursename:"Js in hindi",
+//   price:"300",
+//   courseinstructor:"Hitesh"
+// }
+// const {courseinstructor: instructor}=course
+// console.log(instructor);
+
+// function one(){
+//   const username="hitesh"
+//   function two(){
+//     const website="Youtube"
+//     console.log(username)
+//   }
+//   //console.log(website);
+//   //two();
+// }
+// one();
+
+// const coding=["js","Ruby","java","python","cpp"]
+// const values =coding.forEach((item)=>{
+//   //console.log(item);
+//   return item
+// })
+// console.log(values);
+
+const numps=[1,2,3,4]
+// // const newnum=numps.filter(
+// //   (num)=>{ return num>4}
+// // )
+
+// const newnum=[]
+// numps.forEach((num) => {
+//   if(num>4){
+//     newnum.push(num)
+//   }
+// })
+// console.log(newnum);
+
+// const newNums=numps.map((num)=>num*10).map((num)=>num+1).filter((num)=>num>=40)
+// console.log(newNums);
+
+const mytotal=numps.reduce((acc,curr)=> acc+curr,0)
+console.log(mytotal);
